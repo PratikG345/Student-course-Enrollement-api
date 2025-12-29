8b7950c0ed4ae52f73eb7db8a37df3e7c9ffb8f1
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-12-29 13:55:07 +0530

    added all models

--- course/api/models.py
+++ course/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    max_students = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+class Enrollment(models.Model):
+    STATUS_CHOICES = (
+        ('ACTIVE','ACTIVE'),
+        ('DROPPED','DROPPED'),
+    )
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+    status =  models.CharField(choices=STATUS_CHOICES,max_length=100,default="ACTIVE")
+    
+    class Meta:
+        unique_together = ('student','course')
+    
+    def __str__(self):
+        return f"{self.student.name} - {self.course.title} - {self.status}"
